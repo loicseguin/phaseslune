893131c6014457004126605397780db1167e52db
Loïc Séguin-C <lsc@loicseguin.com>
2014-09-08 23:14:41 -0400

    Toggle visibility of parts

--- phaseslune.js
+++ phaseslune.js
@@ -414,4 +414,22 @@ function pause() {
     window.clearTimeout(timer);
 }
 
+/* Script by: www.jtricks.com 
+ * Version: 20090221 
+ * Latest version: 
+ * www.jtricks.com/javascript/blocks/showinghiding.html 
+ */  
+function togglePageElementVisibility(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    if (obj.style.display == 'none')  
+        obj.style.display = 'block';  
+    else  
+        obj.style.display = 'none';  
+    return false;  
+}  
+  
+
 initFigure();
